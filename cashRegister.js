c36204a4832a0a008f110f809dc70f9260c828c5
Mugwaneza Joseph <muganezajoseph@gmail.com>
2021-07-28 07:17:35 +0200

    cash register function is working well

--- cashRegister.js
+++ cashRegister.js
@@ -1,40 +1,65 @@
-var moneyDict = {
-  "PENNY": 1,
-  "NICKEL": 5,
-  "DIME": 10,
-  "QUARTER": 50,
-  "ONE": 100,
-  "FIVE": 500,
-  "TEN": 1000,
-  "TWENTY": 2000,
-  "ONE HUNDRED": 10000
+const denominations = ['ONE HUNDRED', 'TWENTY', 'TEN', 'FIVE', 'ONE', 'QUARTER', 'DIME', 'NICKEL', 'PENNY'];
+const moneyDict = {
+  'PENNY': 1,
+  'NICKEL': 5,
+  'DIME': 10,
+  'QUARTER': 25,
+  'ONE': 100,
+  'FIVE': 500,
+  'TEN': 1000,
+  'TWENTY': 2000,
+  'ONE HUNDRED': 10000
 };
 
-
-function checkCashRegister(price, cash, cid) {
-    let amtLeftToPay = Math.round((cash - price) * 100);
-    let cidObj = cid.reduce((acc, [moneyType, amt]) =>{
-        return {
-            ...acc,
-            [moneyType]: Math.round(amt * 100)
-        }
-    }, {});
-
-    let moneyToGive = [];
-
-    if(amtLeftToPay >= moneyDict['ONE HUNDRED'] && cidObj['ONE HUNDRED']){
-        if(amtLeftToPay >= cidObj['ONE HUNDRED']){
-            let amtToSubtract = cidObj['ONE HUNDRED'];
-            amtLeftToPay -= amtToSubtract;
-            moneyToGive.push['ONE HUNDRED', amtToSubtract / 100];
-            cidObj['ONE HUNDRED'] = 0;
-        } else {
-            const amtToSubtract = Math.floor((amtLeftToPay/ moneyDict['ONE HUNDRED'])) * moneyDict['ONE HUNDRED'];
-            amtLeftToPay -= amtToSubtract;
-            moneyToGive.push([moneyType, amtToSubtract / 100]);
-            cidObj['ONE HUNDRED'] -= amtToSubtract;
-        }
+const takeOutMoneyFromCid = (amtLeftToPay, cidObj, changeToGive, moneyType) => {
+  if (amtLeftToPay >= moneyDict[moneyType] && cidObj[moneyType]) {
+    if (amtLeftToPay >= cidObj[moneyType]) {
+      const amtToSubtract = cidObj[moneyType];
+      amtLeftToPay -= amtToSubtract;
+      changeToGive.push([moneyType, amtToSubtract / 100]);
+      cidObj[moneyType] = 0;
+    } else {
+      const amtToSubtract =
+        Math.floor(amtLeftToPay / moneyDict[moneyType]) * moneyDict[moneyType];
+      amtLeftToPay -= amtToSubtract;
+      changeToGive.push([moneyType, amtToSubtract/ 100]);
+      cidObj[moneyType] -= amtToSubtract;
     }
   }
-  
-  console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+
+  return [amtLeftToPay, cidObj, changeToGive];
+};
+
+function checkCashRegister(price, cash, cid) {
+  let amtLeftToPay = Math.round((cash - price) * 100);
+
+  let cidObj = cid.reduce((acc, [moneyType, amt]) => {
+    return {
+      ...acc,
+      [moneyType]: Math.round(amt * 100)
+    }
+  }, {});
+
+  let changeToGive = [];
+
+  denominations.forEach(moneyType => {
+    [amtLeftToPay, cidObj, changeToGive] =
+      takeOutMoneyFromCid(amtLeftToPay, cidObj, changeToGive, moneyType);
+  });
+
+  if (amtLeftToPay > 0) {
+    return {status: 'INSUFFICIENT_FUNDS', change: []};
+  }
+
+  const amtLeftInCid = Object.values(cidObj).reduce((acc, amt) => acc + amt, 0);
+
+  if (amtLeftInCid > 0) {
+    return {status: 'OPEN', change: changeToGive};
+  }
+
+  return {status: "CLOSED", change: cid};
+}
+
+
+
+console.log(checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
